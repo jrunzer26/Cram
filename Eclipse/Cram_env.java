1f491f39ffa5dd5f5c077ef4ff0402a2cd8eb7f1
jrunzer26 <jason.runzer@uoit.net>
2015-11-07 11:10:30 -0500

    added environment

---
+++ Eclipse/Cram_env.java
@@ -0,0 +1,20 @@
+
+public class Cram_env {
+	
+	int [][] board = {{0,0,0},{1,1,0},{0,0,0}};
+
+	public static void main(String[] args) {
+
+
+	}
+	
+	public static int [] findMoves(int [][] newBoard){
+		return null;
+		
+	}
+	
+	public calculateWins(int [][] newBoard){
+		return null;
+	}
+
+}
