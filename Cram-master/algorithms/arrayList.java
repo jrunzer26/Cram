e1f31147f31a7d74b0e70037ffd8e542a20fe9e4
jrunzer26 <jason.runzer@uoit.net>
2015-11-21 12:37:11 -0500

    added array list

--- Cram-master/algorithms/arrayList.java
+++ Cram-master/algorithms/arrayList.java
@@ -1,8 +1,10 @@
 package algorithms;
 import org.gamelink.game.Cram;
 import org.gamelink.game.Algo;
+import java.util.ArrayList;
 
-public class recursiveLoss extends Algo{ // Replace TeamName
+
+public class arrayList extends Algo{ // Replace TeamName
     private static String teamName = "recursiveLoss"; // Replace TeamName
 
     public static String getTeamName(){
@@ -11,7 +13,7 @@ public class recursiveLoss extends Algo{ // Replace TeamName
 
     public static void main(String[] args){
     	Cram game = new Cram(false);
-        game.startGame(recursiveLoss.class); // Replace TeamName
+        game.startGame(arrayList.class); // Replace TeamName
     }
 
     public static String algorithm(Cram game){
@@ -23,7 +25,7 @@ public class recursiveLoss extends Algo{ // Replace TeamName
 	//We store the current board each time it is our turn
    int[][] board = game.getBoard();
    //Create an array to store all the move that are possible in the board
-   String [] moves;
+   ArrayList<String> moves;
 
    //Initalize variables
    int count = 0;
@@ -42,18 +44,19 @@ public class recursiveLoss extends Algo{ // Replace TeamName
    //--------------------------------------------------------------------------//
 		
    //Calls the function findMoves with the parameter of the current board and sends the results to the moves array
-   moves = findMoves(board, 38);
+   moves = findMoves(board);
 
    //Creates an array to store the number of losses each move will get
-   int loss[] = new int[38];
-   int lowestMoves[] = new int[38];
+   ArrayList<Integer> loss = new ArrayList<Integer>();
+   ArrayList<Integer> lowestMoves = new ArrayList<Integer>();
    
 
    //Checks to see if there are anymore moves left in the array
   	System.out.println("All Moves:");
-   while(moves[k]!= null)
+    
+   while(k < moves.size())
    {
-   	System.out.println(moves[k]);
+   	System.out.println(moves.get(k));
    	k++;
 
    }
@@ -65,7 +68,7 @@ public class recursiveLoss extends Algo{ // Replace TeamName
    if the number of moves is greater then 24*/
    if(k > 24  || board.length > 5)
    {
-   	String bestMove = moves[0];
+   	String bestMove = moves.get(0);
    	int bestMoveCount = k+1;
 
    	for(int i =0; i < k; i++)
@@ -73,14 +76,14 @@ public class recursiveLoss extends Algo{ // Replace TeamName
    		int j=0;
       
    		
-   		board[(int)moves[i].charAt(0) -48][(int)moves[i].charAt(1)-48] = 1;
-   		board[moves[i].charAt(2) - 48][moves[i].charAt(3) - 48] = 1;
-   		String [] holder = findMoves(board, k);
-   		board[(int)moves[i].charAt(0) -48][(int)moves[i].charAt(1)-48] = 0;
-   		board[moves[i].charAt(2) - 48][moves[i].charAt(3) - 48] = 0;
+   		board[(int)moves.get(i).charAt(0) -48][(int)moves.get(i).charAt(1)-48] = 1;
+   		board[moves.get(i).charAt(2) - 48][moves.get(i).charAt(3) - 48] = 1;
+   		ArrayList<String> holder = findMoves(board);
+   		board[(int)moves.get(i).charAt(0) -48][(int)moves.get(i).charAt(1)-48] = 0;
+   		board[moves.get(i).charAt(2) - 48][moves.get(i).charAt(3) - 48] = 0;
    		
 
-   		while(holder[j] != null)
+   		while(holder.size() > j )
    		{
    			//System.out.print(" "+j);
    			j++;
@@ -89,7 +92,7 @@ public class recursiveLoss extends Algo{ // Replace TeamName
    		if(j < bestMoveCount && j > 0)
    		{
    			bestMoveCount = j;
-   			bestMove = moves[i];   		
+   			bestMove = moves.get(i);   		
    			System.out.println("The number of moves in this board are: "+bestMove);
    		}
 
@@ -101,24 +104,24 @@ public class recursiveLoss extends Algo{ // Replace TeamName
    }
 
    //While there are still moves to check this while loop will keep on executing
-   while(moves[count] != null)
+   while(moves.size() > count)
    {
-   	System.out.println("Analysis Move: " + moves[count]);
+   	System.out.println("Analysis Move: " + moves.get(count));
 
    	//Sets one of the possible moves in the moves array equal to 1 (meaning the spot can no longer be played in)
-   	board[(int)moves[count].charAt(0) -48][(int)moves[count].charAt(1)-48] = 1;
-   	board[moves[count].charAt(2) - 48][moves[count].charAt(3) - 48] = 1;
+   	board[(int)moves.get(count).charAt(0) -48][(int)moves.get(count).charAt(1)-48] = 1;
+   	board[moves.get(count).charAt(2) - 48][moves.get(count).charAt(3) - 48] = 1;
 
 
    	//Enters the calculateLoss function and passes in the board after setting the moves made above to 1 and we pass in true, which repersents it is our turn
-   	loss[count] = calculateLoss(board,true,k);
+   	loss.add(calculateLoss(board,true));
 
    	//The move you previously set to 1 is not set back to 0 (meaning the spot can be played on)
-   	board[(int)moves[count].charAt(0) -48][(int)moves[count].charAt(1)-48] = 0;
-   	board[moves[count].charAt(2) - 48][moves[count].charAt(3) - 48] = 0;
+   	board[(int)moves.get(count).charAt(0) -48][(int)moves.get(count).charAt(1)-48] = 0;
+   	board[moves.get(count).charAt(2) - 48][moves.get(count).charAt(3) - 48] = 0;
 
 			//System.out.println("loss for move: " + count + " = " + loss[count]);
-   	System.out.println("Number of Losses for this Move: " + loss[count]+"\n");
+   	System.out.println("Number of Losses for this Move: " + loss.get(count)+"\n");
 
    	//Increases the counter so next time in the while loop we can check the next possible move in the array and pass the new board into calculateLoss
    	count++;
@@ -126,24 +129,24 @@ public class recursiveLoss extends Algo{ // Replace TeamName
 
 
    //Intializes min to the first value in the loss array
-   int min = loss[0];
+   int min = loss.get(0);
    //Sets the index to the first value in the loss array
    int indexMin = 0;
 
    //Runs though this for loop for the total number of moves in the move array
    for(int i = 0; i < count; i++){
 
-   	if(loss[i] == 0)
+   	if(loss.get(i) == 0)
    	{
-   		return moves[i];
+   		return moves.get(i);
    	}
 
    	//Checks to see if the loss value in the array is less then the min loss value
-   	if (loss[i] <= min){
+   	if (loss.get(i) <= min){
 
 
    			//If it is i/t sets the new minimum loss value to the min value
-   			min = loss[i];
+   			min = loss.get(i);
    			//Stores the index of the loss array so we can later call it to play the move from the moves array
    			indexMin = i;
    	
@@ -153,7 +156,7 @@ public class recursiveLoss extends Algo{ // Replace TeamName
    }
 
    //Plays the move with the fewest losses
-   return moves[indexMin];
+   return moves.get(indexMin);
 
 
 }
@@ -163,13 +166,13 @@ public class recursiveLoss extends Algo{ // Replace TeamName
  * The purpose of this function is it plays though every permutation of the move we passed into the board, it does this by checking to see how many losses the move we played
  * will have and then we return number of losses into the loss array at the index that corresponds to the move we just played from the moves array
  */
-public static int calculateLoss(int [][] newBoard,boolean turn, int size){
+public static int calculateLoss(int [][] newBoard,boolean turn){
 
 	//The variable for the number of losses
 	int loss = 0;
 
 	//Stores all the possible moves into the movoes array
-	String [] moves = findMoves(newBoard, size);
+	ArrayList<String> moves = findMoves(newBoard);
 		
 		//int i = 0;
 
@@ -179,7 +182,7 @@ public static int calculateLoss(int [][] newBoard,boolean turn, int size){
 		}*/
 
 		//If there are no moves in the array enter the if statement
-		if(moves[0] == null){
+		if(moves.size() == 0){
 			//If we are not the last people to play then return 1 (which in this case repersents a loss)
 			if(!turn){
 				return 1;
@@ -202,13 +205,13 @@ public static int calculateLoss(int [][] newBoard,boolean turn, int size){
 		int index = 0;
 
 		//Same while loop as before, we start recursion, by passing in the next moved to be played on the board we passed into the calculateLoss function
-		while(moves[index] != null){
+		while(moves.size() > index){
 			//System.out.println("got in the while loop of calculate moves");
-			newBoard[moves[index].charAt(0) - 48][moves[index].charAt(1) - 48] = 1;
-			newBoard[moves[index].charAt(2) -48][moves[index].charAt(3) - 48] = 1;
-			loss += calculateLoss (newBoard,turn,size-1);
-			newBoard[moves[index].charAt(0) - 48][moves[index].charAt(1) - 48] = 0;
-			newBoard[moves[index].charAt(2) -48][moves[index].charAt(3) - 48] = 0;
+			newBoard[moves.get(index).charAt(0) - 48][moves.get(index).charAt(1) - 48] = 1;
+			newBoard[moves.get(index).charAt(2) -48][moves.get(index).charAt(3) - 48] = 1;
+			loss += calculateLoss (newBoard,turn);
+			newBoard[moves.get(index).charAt(0) - 48][moves.get(index).charAt(1) - 48] = 0;
+			newBoard[moves.get(index).charAt(2) -48][moves.get(index).charAt(3) - 48] = 0;
 			index++;
 		}
 		//After you play through the whole game with the first move you sent in, it will return the amount of times you could lose with the move
@@ -219,7 +222,7 @@ public static int calculateLoss(int [][] newBoard,boolean turn, int size){
 	 *
 	 *
 	 */
-	public static String [] findMoves(int [][] newBoard, int size){
+	public static ArrayList<String> findMoves(int [][] newBoard){
 		String previous = null;
 		String current = null;
 		int previousJV = 99;
@@ -227,7 +230,7 @@ public static int calculateLoss(int [][] newBoard,boolean turn, int size){
 		int counterIndex = 0;
 
 		
-		String []totalMoves = new String[size];
+    ArrayList<String> totalMoves = new ArrayList<String>();
 
 
 		for(int i = 0; i < (newBoard.length); i++){
@@ -235,8 +238,7 @@ public static int calculateLoss(int [][] newBoard,boolean turn, int size){
 				if(newBoard[j][i] == 0){
 					current = Integer.toString(j) + Integer.toString(i);
 					if((previous != null) && (j-previousJV == 1) ){
-						totalMoves[counterIndex] = previous + current;
-						counterIndex++;
+						totalMoves.add(previous + current);
 					}
 				}
 				else{
@@ -252,8 +254,7 @@ public static int calculateLoss(int [][] newBoard,boolean turn, int size){
 				if(newBoard[i][j] == 0){
 					current = Integer.toString(i) + Integer.toString(j);
 					if((previous != null) && (j-previousJH == 1) ){
-						totalMoves[counterIndex] = previous + current;
-						counterIndex++;
+					   totalMoves.add(previous + current);
 					}
 				}
 				else{
---
+++ Cram-master/arrayList.bat
@@ -0,0 +1,4 @@
+SET teamname=arrayList
+javac -cp Cram.jar algorithms/%teamname%.java
+java -cp Cram.jar;. algorithms.%teamname%
+pause
